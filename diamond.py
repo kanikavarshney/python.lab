619be9b6d95075c3d9527643b3ac0f198f569344
kanikavarshney <60420501+kanikavarshney@users.noreply.github.com>
2020-02-05 10:38:12 +0530

    pattern programs

---
+++ diamond.py
@@ -0,0 +1,8 @@
+x = int(input("enter the height"))
+for i in range (h):
+     for j in range(h):
+          if i==0 or j==0 or i==h-1 or j==h-1:
+               print("*",end="")
+          else:
+          
+     
